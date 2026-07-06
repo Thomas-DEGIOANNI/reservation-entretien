6d51e3a57212516774280104d40a88fa01c41281
Thomas-DEGIOANNI <t.degioanni@smdea09.fr>
2026-07-06 15:16:27 +0200

    Correction login admin

--- scripts/create-admin.ts
+++ scripts/create-admin.ts
@@ -6,14 +6,20 @@ const prisma = new PrismaClient();
 async function main() {
   const password = await bcrypt.hash("Admin123!", 10);
 
-  await prisma.admin.create({
-    data: {
+  await prisma.admin.upsert({
+    where: {
+      email: "admin@smdea.fr",
+    },
+    update: {
+      password,
+    },
+    create: {
       email: "admin@smdea.fr",
       password,
     },
   });
 
-  console.log("✅ Administrateur créé !");
+  console.log("✅ Administrateur créé ou mis à jour !");
 }
 
 main()
